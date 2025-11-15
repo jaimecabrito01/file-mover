df08ecd12be5d142e924a92a7be641ab35cf005e
jaimecabrito01 <azevedocarneirojaimevictor@gmail.com>
2025-11-15 20:53:42 -0300

    feat: leitura de config e nova implementação

--- internal/organizer/organizer.go
+++ internal/organizer/organizer.go
@@ -2,31 +2,37 @@ package organizer
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/fsnotify/fsnotify"
-	"github.com/jaimecabrito01/separador-arquivos-service/entities"
+	
 	"os"
 )
 
+func LoadConfig() (string, string, string, string, string, error) {
 
-data,_:= os.ReadFile("../../config.json")
-var m map[string]interface{}
-json.Unmarshal(data,&m)
+	// Lê o arquivo config.json
+	data, err := os.ReadFile("../../config.json")
+	if err != nil {
+		return "", "", "", "", "", err
+	}
 
-downloads:= m["downloads"]
-musics := m["musics"]
-videos := m["videos"]
-documents := m["documents"]
-images := m["images"]
- 
-watcher,err := fsnotify.NewWatcher()
-if err != nil {
-	log.Fatal(err)
+	var m map[string]interface{}
+	err = json.Unmarshal(data, &m)
+	if err != nil {
+		return "", "", "", "", "", err
+	}
+
+	// Pega os valores do mapa como string
+	downloads, _ := m["downloads"].(string)
+	musics, _ := m["musics"].(string)
+	videos, _ := m["videos"].(string)
+	documents, _ := m["documents"].(string)
+	images, _ := m["images"].(string)
+
+	return downloads, musics, videos, documents, images, nil
+}
+
+
+func MoveVideo(path *string,new *string){
+
+	os.Rename(*path,*new)
 
 }
-defer watcher.Close()
-
-go func(){
-	
-}
-
---
+++ internal/watcher/watcher.go
@@ -0,0 +1,70 @@
+package watcher
+
+import (
+	"fmt"
+	"log"
+
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/jaimecabrito01/separador-arquivos-service/entities"
+	"github.com/jaimecabrito01/separador-arquivos-service/internal/organizer"
+)
+
+
+
+
+
+
+func Watcher(){
+
+downloads, musics, videos, documents, images, err := organizer.LoadConfig()
+if err != nil {
+    log.Fatal(err)
+}
+
+watcher,err := fsnotify.NewWatcher()
+if err != nil {
+	log.Fatal(err)
+
+}
+
+
+defer watcher.Close()
+done := make(chan bool)
+
+go func(){
+	for {
+		select{
+		case event,ok := <- watcher.Events:
+			if !ok{
+				return
+			}
+		if event.Op&fsnotify.Write == fsnotify.Write{
+			ext := filepath.Ext(event.Name)
+				for _, extVideo := range entities.VideoExtensions{
+					if ext == string(extVideo){
+						organizer.MoveVideo(&event.Name,&videos)
+					}
+				}
+				for _, extDocs := range entities.DocumentExtensions{
+					if ext == string(extDocs){
+						
+					}
+				}
+			}
+		case err,ok := <- watcher.Errors:
+			if !ok{
+				return
+			}
+			log.Println("error:",err)
+		}
+	}
+}()
+err = watcher.Add(downloads)
+<- done
+}
+
+
+
+
