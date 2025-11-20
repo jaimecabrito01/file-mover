c11a38b89ae0e61401402fb7f99115d7ceab125d
jaimecabrito01 <azevedocarneirojaimevictor@gmail.com>
2025-11-20 17:27:53 -0300

    fix:criação de config.json e execução como daemon

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/jaimecabrito01/separador-arquivos-service/internal/tela"
 	"github.com/jaimecabrito01/separador-arquivos-service/internal/watcher"
 )
 
 func main() {
-	tela.Init()
+	setup := flag.Bool("setup", false, "configure paths")
+	flag.Parse()
+
+	if *setup {
+		tela.Init()
+		return
+	}
+
 	watcher.Watcher()
 }
